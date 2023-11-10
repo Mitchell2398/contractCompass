c457e22218f46b398bfa5c6be211de56e0a2236a
Jack Mitchell <jackmitchell2398@gmail.com>
2023-11-10 19:03:32 +0000

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAGTK4q66gt-GBGgy1LgE8slZpQ2KPgHlY",
+  authDomain: "contract-compass.firebaseapp.com",
+  projectId: "contract-compass",
+  storageBucket: "contract-compass.appspot.com",
+  messagingSenderId: "889246507726",
+  appId: "1:889246507726:web:e032bc2e57dc25529fa69f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
